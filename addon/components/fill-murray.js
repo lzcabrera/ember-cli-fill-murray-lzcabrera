d285bbd5600ab4258319b4668e0e326dd666fe34
Laura Cabrera <lzcabrera@gmail.com>
2015-04-25 11:02:32 -0700

    customize fill-murray component

---
+++ addon/components/fill-murray.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+import layout from '../templates/components/fill-murray';
+
+export default Ember.Component.extend({
+  layout: layout,
+  height: 100,
+  width: 100,
+  src: Ember.computed('height', 'width', function(){
+    var base = 'http://www.fillmurray.com/';
+    return base + this.get('width') + '/' + this.get('height');
+  })
+});
---
+++ app/components/fill-murray.js
@@ -0,0 +1,3 @@
+import fillMurray from 'ember-cli-fill-murray/components/fill-murray';
+
+export default fillMurray;
