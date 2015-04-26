15eb2027ae0cd41c9cea18959c0143f0b749a3e3
Laura Cabrera <lzcabrera@gmail.com>
2015-04-25 21:44:45 -0700

    move code back to addon and add htmlbars dependency

--- addon/components/fill-murray.js
+++ addon/components/fill-murray.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import layout from '../templates/components/fill-murray';
 
 export default Ember.Component.extend({
-  layout: layout
+  layout: layout,
+  height: 100,
+  width: 100,
+  src: Ember.computed('height', 'width', function(){
+    var base = 'http://www.fillmurray.com/';
+    return base + this.get('width') + '/' + this.get('height');
+  })
 });
--- app/components/fill-murray.js
+++ app/components/fill-murray.js
@@ -1,10 +1,3 @@
-import Ember from 'ember';
+import fillMurray from 'ember-cli-fill-murray/components/fill-murray';
 
-export default Ember.Component.extend({
-  height: 100,
-  width: 100,
-  src: Ember.computed('height', 'width', function(){
-    var base = 'http://www.fillmurray.com/';
-    return base + this.get('width') + '/' + this.get('height');
-  })
-});
+export default fillMurray;
